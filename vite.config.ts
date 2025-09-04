0bc309f8a156ed6e8073bce4cd931aace3f1d901
Anna <101081715+yoonajung@users.noreply.github.com>
2025-09-04 09:59:47 +0900

    Update vite.config.ts
    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      plugins: [react()],
-      base: '/Children-s-Story-Book-Generator/', // GitHub Pages용
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY || env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_API_KEY || env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
-      build: {
-        outDir: 'dist'
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/Children-s-Story-Book-Generator/', // 리포 이름 그대로
+})
